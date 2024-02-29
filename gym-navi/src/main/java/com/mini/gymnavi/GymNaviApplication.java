da92a8ed416e4e0716ff51cdc888813c85830a79
JoHyunwoo <jhw3874@gmail.com>
2024-02-29 15:40:11 +0900

    first commit

---
+++ gym-navi/src/main/java/com/mini/gymnavi/GymNaviApplication.java
@@ -0,0 +1,13 @@
+package com.mini.gymnavi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymNaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymNaviApplication.class, args);
+	}
+
+}
---
+++ gym-navi/src/test/java/com/mini/gymnavi/GymNaviApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.gymnavi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymNaviApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
